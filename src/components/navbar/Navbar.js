fc331d284b78a939c7d1a545075e8b90d4211906
Hadjer SAHARI <h.sahari@meteo.dz>
2023-01-05 16:19:17 +0100

    new modif navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import "./Navbar.scss"
+import Style from './Navbar.module.scss';
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-       <div className='link'>home</div>
-       <div className='link'>gg</div>
-       <div className='link'>gggggggf</div>
-
-    </div>
+    <nav > 
+        <ul className='navbar'>
+          <li className='list-item'><a className='link' href='/'>home</a></li>
+          <li className='list-item'><a className='link' href='/about'>about me</a></li>
+          <li className='list-item'><a className='link' href='/'></a></li>
+          <li className='list-item'><a className='link' href='/'>portfolio</a></li>
+          <li className='list-item'><a className='link' href='/'>ll</a></li>
+        </ul>
+    </nav>
   )
 }
 
--- src/container/Layout.js
+++ src/container/Layout.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../components/home/Home';
 import About from '../components/about/About';
+import Navbar from '../components/navbar/Navbar';
 
 const Layout = () => {
   return (
     <div>
+      <Navbar />
         <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/about" element={<About />}></Route>
